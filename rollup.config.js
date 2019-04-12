a23240c8dc4e13a2684c8352e126d0908285572c
GorrilaBeanz <34932278+GorrilaBeanz@users.noreply.github.com>
2019-04-11 23:13:15 -0400

    fix configs

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import babel from 'rollup-plugin-babel';
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
+import postcss from 'rollup-plugin-postcss';
 import { sizeSnapshot } from 'rollup-plugin-size-snapshot';
 
 export default {
@@ -9,12 +10,16 @@ export default {
     file: 'dist/cjs.js',
     format: 'cjs'
   },
+  external: ['react', 'crypto'],
   plugins: [
     babel({
       exclude: 'node_modules/**'
     }),
+    postcss({
+      modules: true
+    }),
     resolve(),
-    commonjs(),
-    sizeSnapshot()
+    sizeSnapshot(),
+    commonjs()
   ]
 };
